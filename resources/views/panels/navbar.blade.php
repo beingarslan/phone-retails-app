9068a722d78395d319daa3073507662697a786de
ARSLAN ASLAM <39445256+beingarslan@users.noreply.github.com>
2022-02-16 00:58:54 +0500

    Update navbar.blade.php

--- resources/views/panels/navbar.blade.php
+++ resources/views/panels/navbar.blade.php
@@ -22,28 +22,8 @@
 
       </div>
       <ul class="nav navbar-nav align-items-center ms-auto">
-        <!-- <li class="nav-item dropdown dropdown-language">
-          <a class="nav-link dropdown-toggle" id="dropdown-flag" href="#" data-bs-toggle="dropdown" aria-haspopup="true">
-            <i class="flag-icon flag-icon-us"></i>
-            <span class="selected-language">English</span>
-          </a>
-          <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-flag">
-            <a class="dropdown-item" href="{{ url('lang/en') }}" data-language="en">
-              <i class="flag-icon flag-icon-us"></i> English
-            </a>
-            <a class="dropdown-item" href="{{ url('lang/fr') }}" data-language="fr">
-              <i class="flag-icon flag-icon-fr"></i> French
-            </a>
-            <a class="dropdown-item" href="{{ url('lang/de') }}" data-language="de">
-              <i class="flag-icon flag-icon-de"></i> German
-            </a>
-            <a class="dropdown-item" href="{{ url('lang/pt') }}" data-language="pt">
-              <i class="flag-icon flag-icon-pt"></i> Portuguese
-            </a>
-          </div>
-        </li> -->
-        <livewire:settings.theme.button :configData="$configData" />
 
+        <livewire:settings.theme.button :configData="$configData" />
 
         <li class="nav-item dropdown dropdown-user">
           <a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="javascript:void(0);" data-bs-toggle="dropdown" aria-haspopup="true">
@@ -69,10 +49,10 @@
             $name =substr($name,0,2);
             @endphp
             @endif
-            
-               
-           
-            <div class="avatar bg-light-success avatar-lg">
+
+
+
+            <div class="avatar bg-light-success">
               <span class="avatar-content">{{$name}}</span>
             </div>
           </a>
